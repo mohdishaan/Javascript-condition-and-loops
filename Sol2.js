852f2defa6b5a1a2d5422ff5cb7025240b82d43c
Mohammad Ishaan <mohammad.ishaan04@gmail.com>
2024-10-10 11:53:52 +0530

    Commit successfull

---
+++ Sol2.js
@@ -0,0 +1,15 @@
+const marks = 96;
+
+if(marks > 90){
+    console.log("Student gets A Grade");
+}
+else if (marks > 70){
+    console.log("Student gets B Grade");
+}
+else if (marks > 50){
+    console.log("Student gets C Grade");
+}
+else{
+    console.log("Student gets F Grade");
+}
+
---
+++ Sol5.js
@@ -0,0 +1,12 @@
+console.log("Printing numbers from 1 to 25 in ascending order:");
+let i=1;
+while(i<26){
+    console.log(i); 
+    i++;
+}
+
+console.log("Printing numbers from 1 to 25 in descending order:");
+while(i>=2){
+    i--;
+    console.log(i); 
+}
